66c1d2e2014aa94e722ab7c7fd18911011f65e01
Maanux <emanuel.skanis@hotmail.com>
2025-11-03 15:49:21 -0300

    Rota ping pong para teste

--- src/main/java/SimulaEnem/infra/security/SecurityConfig.java
+++ src/main/java/SimulaEnem/infra/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login", "/auth/register").permitAll()
+                        .requestMatchers("/auth/login", "/auth/register", "/ping").permitAll()
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
