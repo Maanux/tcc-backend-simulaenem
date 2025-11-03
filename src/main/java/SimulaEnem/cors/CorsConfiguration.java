da987b166c9d352c68939b96875f733ef6675117
Maanux <emanuel.skanis@hotmail.com>
2025-11-02 23:04:10 -0300

    Arrumando cors para prod

--- src/main/java/SimulaEnem/cors/CorsConfiguration.java
+++ src/main/java/SimulaEnem/cors/CorsConfiguration.java
@@ -11,11 +11,12 @@ public class CorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
+                        "http://localhost:4000",
                         "http://localhost:4200",
                         "http://127.0.0.1:5500",
                         "http://localhost:8080"
+
                 )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowedHeaders("*")
                 .allowCredentials(true)
